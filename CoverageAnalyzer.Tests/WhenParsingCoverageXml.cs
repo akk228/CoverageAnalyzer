22fa255a877e0295751ecd21df1b4bd2f313af23
Andrei Kovtun <andreykpq@gmail.com>
2025-08-13 03:42:10 +0200

    sets up first test

---
+++ CoverageAnalyzer.Tests/WhenParsingCoverageXml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using CoverageAnalyzer.CoverageReportParser.Entitites;
+
+namespace CoverageAnalyzer.Tests;
+
+public class WhenParsingCoverageXml
+{
+    public static IEnumerable<object[]> TestFilesData()
+    {
+        yield return new object[] {
+            new List<FileCoverage>
+            {
+                new FileCoverage { UId = 1, FileName = "TestProject1.cs" },
+                new FileCoverage { UId = 2, FileName = "TestProject2.cs" },
+                new FileCoverage { UId = 3, FileName = "TestProject3.cs" }
+            },
+            "TestFiles/test-xunit-coverage.xml"
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(TestFilesData))]
+    public void GetCorrectFileNamesAndIds(IEnumerable<FileCoverage> expectedFileCoverages, string coverageFilePath)
+    {
+        var parser = new CoverageReportParser();
+        var actualFileCoverages = parser.ParseCoverageReport(coverageFilePath);
+
+        Assert.Equal(expectedFileCoverages.Count(), actualFileCoverages.Count());
+
+        foreach (var expected in expectedFileCoverages)
+        {
+            var actual = actualFileCoverages.FirstOrDefault(fc => fc.UId == expected.UId);
+            Assert.NotNull(actual);
+            Assert.Equal(expected.FileName, actual.FileName);
+        }
+    }
+}
