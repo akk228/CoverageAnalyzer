c0d28b109dded790ca2af4e329d037c206f797d3
Andrei Kovtun <andreykpq@gmail.com>
2025-08-13 03:26:32 +0200

    adds an Id for the coverable file

--- CoverageAnalyzer/ReportParser/Entities/FileCoverage.cs
+++ CoverageAnalyzer/ReportParser/Entities/FileCoverage.cs
@@ -4,6 +4,7 @@ namespace CoverageAnalyzer.CoverageReportParser.Entitites
 {
     public class FileCoverage
     {
+        public int UId { get; set; }
         public string FileName { get; set; }
         public IEnumerable<CoverableLine> Lines { get; set; }
     }
