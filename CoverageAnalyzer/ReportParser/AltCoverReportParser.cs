698a3dc50f30d47854b4b8c76a5a766391f901a8
Andrei Kovtun <andreykpq@gmail.com>
2025-08-13 16:19:43 +0200

    adds method description

--- CoverageAnalyzer/ReportParser/AltCoverReportParser.cs
+++ CoverageAnalyzer/ReportParser/AltCoverReportParser.cs
@@ -19,6 +19,12 @@ namespace CoverageAnalyzer.ReportParser
             return fileCoverage;
         }
 
+        /// <summary>
+        /// Extracts file coverage information from the XML document.
+        /// This method retrieves all <File> elements and constructs FileCoverage objects for each.
+        /// </summary>
+        /// <param name="document">Xml with code coverage report</param>
+        /// <returns>enunmerable colection of files that are subjected to coverage</returns>
         private IEnumerable<FileCoverage> GetFiles(XDocument document)
         {
             // TODO: Handle potential null values and exceptions
