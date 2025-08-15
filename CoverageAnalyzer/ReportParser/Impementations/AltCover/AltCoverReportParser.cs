08b288d6394632007d000335b4f0e62ec4e9fafa
Andrei Kovtun <andreykpq@gmail.com>
2025-08-15 13:04:54 +0200

    Updates parser to use generic method for xml parsing

--- CoverageAnalyzer/ReportParser/Impementations/AltCover/AltCoverReportParser.cs
+++ CoverageAnalyzer/ReportParser/Impementations/AltCover/AltCoverReportParser.cs
@@ -4,60 +4,29 @@ using System.Xml.Linq;
 using System.Xml.Serialization;
 using CoverageAnalyzer.ReportParser.Entitites;
 using CoverageAnalyzer.ReportParser.Impementations.AltCover.XmlSchema;
+using CoverageAnalyzer.ReportParser.XmlReader;
 
 namespace CoverageAnalyzer.ReportParser.Implementations.AltCover
 {
     public class AltCoverReportParser : IReportParser
     {
-        private const string UIdAttributeName = "uid";
-        private const string FullPathAttributeName = "fullPath";
         private const string FileNodeName = "File";
         private const string SequenceNodeName = "SequencePoints";
+
+        private readonly DescendantReader _descendantReader;
+        
+        public AltCoverReportParser(DescendantReader descendantReader)
+        {
+            _descendantReader = descendantReader;
+        }
 
         public IEnumerable<FileCoverage> ParseCoverageReport(string coverageFilePath)
         {
             var coverageReport = XDocument.Load(coverageFilePath);
-            var fileCoverage = GetFiles(coverageReport);
-            var fileIds = fileCoverage.ToDictionary(f => f.UId, f => f);
-
-            return fileCoverage;
-        }
-
-        /// <summary>
-        /// Extracts file coverage information from the XML document.
-        /// This method retrieves all <File> elements and constructs FileCoverage objects for each.
-        /// </summary>
-        /// <param name="document">Xml with code coverage report</param>
-        /// <returns>enunmerable colection of files that are subjected to coverage</returns>
-        private IEnumerable<FileCoverage> GetFiles(XDocument document)
-        {
-            // TODO: Handle potential null values and exceptions
-            return document
-                .Descendants(FileNodeName)
-                .Select(file => new FileCoverage(
-                    int.Parse(file.Attribute(UIdAttributeName).Value),
-                    file.Attribute(FullPathAttributeName).Value
-                ));
-        }
-
-        /// <summary>
-        /// Extracts sequence points from the XML document.
-        /// This method retrieves all <SequencePoints> elements and deserializes them into SequencePoint objects.
-        /// </summary>
-        /// <param name="document">Xml with code coverage report</param>
-        /// <returns>enunmerable colection of sequence points</returns>
-        private IEnumerable<SequencePoint> GetSequencePoints(XDocument document)
-        {
-            var sequencePointSerializer = new XmlSerializer(typeof(SequencePoint));
-            return document
-                .Descendants(SequenceNodeName)
-                .Select(sq =>
-                {
-                    using (var reader = sq.CreateReader())
-                    {
-                        return (SequencePoint)sequencePointSerializer.Deserialize(reader);
-                    }
-                });
+            var filesSubjectedToCoverage = _descendantReader.ReadDocumentDescendants<File>(coverageReport, FileNodeName);
+            var sequencePoints = _descendantReader.ReadDocumentDescendants<SequencePoint>(coverageReport, SequenceNodeName);
+            
+            return filesSubjectedToCoverage.Select(file => new FileCoverage(file.UId, file.FullPath)); ;
         }
     }
 }
