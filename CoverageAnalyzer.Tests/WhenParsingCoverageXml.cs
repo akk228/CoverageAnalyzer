f193bfb9a86b02d671e13bc2035fa39b50016534
Andrei Kovtun <andreykpq@gmail.com>
2025-08-15 13:40:37 +0200

    Adds test for xml parser

--- CoverageAnalyzer.Tests/WhenParsingCoverageXml.cs
+++ CoverageAnalyzer.Tests/WhenParsingCoverageXml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using CoverageAnalyzer.ReportParser;
 using CoverageAnalyzer.ReportParser.Entitites;
 using CoverageAnalyzer.ReportParser.Implementations.AltCover;
 using CoverageAnalyzer.ReportParser.XmlReader;
@@ -8,6 +9,8 @@ namespace CoverageAnalyzer.Tests;
 
 public class WhenParsingCoverageXml
 {
+    private readonly IReportParser _reportParser;
+
     public static IEnumerable<object[]> TestFilesData()
     {
         yield return new object[] {
@@ -19,12 +22,16 @@ public class WhenParsingCoverageXml
         };
     }
 
+    public WhenParsingCoverageXml()
+    {
+        _reportParser = new AltCoverReportParser(new DescendantReader());
+    }
+
     [Theory]
     [MemberData(nameof(TestFilesData))]
     public void GetCorrectFileNamesAndIds(IEnumerable<FileCoverage> expectedFileCoverages, string coverageFilePath)
     {
-        var parser = new AltCoverReportParser(new DescendantReader());
-        var actualFileCoverages = parser.ParseCoverageReport(coverageFilePath);
+        var actualFileCoverages = _reportParser.ParseCoverageReport(coverageFilePath);
 
         Assert.Equal(expectedFileCoverages.Count(), actualFileCoverages.Count());
 
@@ -33,6 +40,44 @@ public class WhenParsingCoverageXml
             var actual = actualFileCoverages.FirstOrDefault(fc => fc.UId == expected.UId);
             Assert.NotNull(actual);
             Assert.Equal(expected.FileName, actual.FileName);
+        }
+    }
+
+    public static IEnumerable<object[]> TestLinesData()
+    {
+        yield return new object[] {
+            new List<CoverableLine>
+            {
+                new CoverableLine(8, 1),
+                new CoverableLine(9, 1),
+                new CoverableLine(10, 0),
+                new CoverableLine(11, 0),
+                new CoverableLine(14, 1),
+                new CoverableLine(16, 1),
+                new CoverableLine(17, 1),
+            },
+            "TestFiles/test-xunit-coverage.xml"
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(TestLinesData))]
+    public void GetCorrectLines(IEnumerable<CoverableLine> expectedLines, string coverageFilePath)
+    {
+        var actualFileCoverages = _reportParser.ParseCoverageReport(coverageFilePath);
+
+        var actualLines = actualFileCoverages
+            .SelectMany(fc => fc.Lines)
+            .OrderBy(line => line.LineNumber)
+            .ToList();
+
+        Assert.Equal(expectedLines.Count(), actualLines.Count);
+
+        foreach (var expected in expectedLines)
+        {
+            var actual = actualLines.FirstOrDefault(line => line.LineNumber == expected.LineNumber);
+            Assert.NotNull(actual);
+            Assert.Equal(expected.IsCovered, actual.IsCovered);
         }
     }
 }
