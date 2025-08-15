c6e6b59cf9445622c8bc34f38a989c94fe2fcd5a
Andrei Kovtun <andreykpq@gmail.com>
2025-08-15 13:28:55 +0200

    Adds constructor to coverable line

--- CoverageAnalyzer/ReportParser/Entities/CoverableLine.cs
+++ CoverageAnalyzer/ReportParser/Entities/CoverableLine.cs
@@ -2,7 +2,13 @@ namespace CoverageAnalyzer.ReportParser.Entitites
 {
     public class CoverableLine
     {
-        public int LineNumber { get; set; }
-        public bool IsCovered { get; set; }
+        public CoverableLine(int lineNumber, int hitCount)
+        {
+            LineNumber = lineNumber;
+            IsCovered = hitCount > 0;
+        }
+
+        public int LineNumber { get; }
+        public bool IsCovered { get; }
     }
 }
