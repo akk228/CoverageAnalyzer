aafea21f4e3c4e9f8ec60b7e852cbd8662778115
Andrei Kovtun <andreykpq@gmail.com>
2025-08-16 16:17:17 +0200

    adds test of git reader

---
+++ CoverageAnalyzer.Tests/WhenFetchingGitDiff.cs
@@ -0,0 +1,18 @@
+using System;
+using CoverageAnalyzer.GitApi;
+using Xunit;
+
+namespace CoverageAnalyzer.Tests
+{
+    public class WhenFetchingGitDiff
+    {
+        [Theory]
+        [InlineData(@"C:\Users\andre\Documents\Studying\CodeCoverageTrial", "caclulator-change", "main")]
+        public void ShouldThrowExceptionForInvalidRepositoryPath(string repoPath, string targetBranch, string referenceBranch)
+        {
+            var diffReader = new DiffReader();
+            var result = diffReader.GetDiff(repoPath, targetBranch, referenceBranch);
+            Assert.True(true, "This test is a placeholder for actual implementation.");
+        }
+    }
+}
